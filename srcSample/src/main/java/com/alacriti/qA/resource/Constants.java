20d407f41bf799237b3163826e70ad564d7a3887
Parvez Khusro <park3@outlook.com>
2018-02-18 02:19:44 +0530

    Fixed

--- srcSample/src/main/java/com/alacriti/qA/resource/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.alacriti.qA.resource;
-
-public class Constants {
-	public  final static String MANAGEMENT="Management";
-	public static final String ALACRITI_PROJ= "Alacriti";
-	public static final int ACTIVE_ACCOUNT = 1; 
-
-}
